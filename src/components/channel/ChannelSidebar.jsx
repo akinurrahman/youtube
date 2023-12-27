edd73f52fbe46109b3a1d150fa9952e2c2a8e7fd
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-12-27 13:35:17 +0530

    channel sidebar : added a underline whichever channelpage is active

--- src/components/channel/ChannelSidebar.jsx
+++ src/components/channel/ChannelSidebar.jsx
@@ -1,83 +1,46 @@
-import { NavLink, useParams } from "react-router-dom";
 import React from "react";
+import { NavLink, useParams, useLocation } from "react-router-dom";
 
 const ChannelSidebar = () => {
   const { channelId } = useParams();
+  const location = useLocation();
 
-  // Removing scrollbar
-  const scrollContainerStyle = {
-    overflowX: "auto",
-    scrollbarWidth: "none",
-    msOverflowStyle: "none",
+  // Helper function to determine if a NavLink should be active
+  const isActive = (path) => {
+    return location.pathname === `/channel/${channelId}${path}`;
   };
+
+  // Data for NavLinks: label and path
+  const navLinks = [
+    { label: "Home", path: "" },
+    { label: "Videos", path: "/videos" },
+    { label: "Shorts", path: "/shorts" },
+    { label: "Live", path: "/live" },
+    { label: "Playlist", path: "/playlist" },
+    { label: "Search", path: "/search" },
+    { label: "Community", path: "/community" },
+  ];
+
+  // Apply active style conditionally
+  const getNavLinkStyle = (path) => {
+    return isActive(path)
+      ? { borderBottom: "2px solid blue", color: "blue" }
+      : {};
+  };
+
   return (
-    <ul
-      className="mx-2 flex justify-between space-x-7 overflow-x-auto sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5"
-      style={scrollContainerStyle}
-    >
-      <li>
-        <NavLink
-          to={`/channel/${channelId}`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/videos`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Videos
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/shorts`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Shorts
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/live`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Live
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/playlist`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Playlist
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/search`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Search
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/channel/${channelId}/community`}
-          // activeClassName="border-b-2 border-black text-black"
-          className="cursor-pointer text-gray-800"
-        >
-          Community
-        </NavLink>
-      </li>
+    <ul className="mx-2 flex justify-between space-x-7 overflow-x-auto sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+      {navLinks.map((link, index) => (
+        <li key={index}>
+          <NavLink
+            to={`/channel/${channelId}${link.path}`}
+            className="cursor-pointer text-gray-800"
+            style={getNavLinkStyle(link.path)}
+          >
+            {link.label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 };
