445fa85bf6a3e30cb1c9a3c2c92d01e7b8dced21
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-12-29 16:21:53 +0530

    mobile search ui done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { IoIosSearch, IoIosNotificationsOutline } from "react-icons/io";
 import { FaRegWindowClose } from "react-icons/fa";
-
+import { IoArrowBackSharp } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import { BsMicFill } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Hamburger from "./Hamburger";
@@ -9,6 +10,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchQuery } from "../../redux/features/infoSlice";
 import Suggestions from "../search/Suggestions";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
@@ -34,8 +36,12 @@ const Header = () => {
   };
 
   return (
-    <div>
-      <div className="fixed left-0 top-0 z-30 flex w-full items-center justify-between bg-white  px-4 py-3 ">
+    <div className="h-10 md:h-12">
+      <div
+        className={`fixed left-0 top-0 z-30 flex w-full items-center justify-between   px-4 py-3  ${
+          mobileSearch && "hidden"
+        }`}
+      >
         {/* Column 1 */}
         <div className={` flex  space-x-9 ${menu && " space-x-9 "} `}>
           {menu ? (
@@ -112,17 +118,28 @@ const Header = () => {
         {menu && <Hamburger />}
       </div>
       {/* this part is for mobile device only */}
-      <div className={`z-50 mx-4 mt-2  ${!mobileSearch && "hidden"}`}>
-        <input
-          type="text"
-          className="w-full rounded-md border border-gray-400 px-2  py-[2px] outline-none focus:border-gray-600 "
-          placeholder="Search"
-          onChange={(e) => dispatch(setSearchQuery(e.target.value))}
-          value={searchQuery}
-          onKeyDown={handleKeyDown}
+      <div
+        className={`z-50  mx-4   mt-2 flex  h-9 items-center rounded-3xl ${
+          !mobileSearch && "hidden"
+        }`}
+      >
+        <IoIosArrowRoundBack
+          size={40}
+          onClick={() => setMobileSearch((prev) => !prev)}
         />
+        <div className="ml-2 flex h-full w-full items-center space-x-3 rounded-full border-2 bg-gray-200 pr-3">
+          <input
+            type="text"
+            className="w-4/5 rounded-md bg-transparent  px-4  py-[2px] outline-none focus:border-gray-600 "
+            placeholder="Search"
+            onChange={(e) => dispatch(setSearchQuery(e.target.value))}
+            value={searchQuery}
+            onKeyDown={handleKeyDown}
+          />
+          <IoMdClose size={22} onClick={() => dispatch(setSearchQuery(""))} className="bg-red-300 cursor-pointer" />
+          <IoIosSearch size={22} onClick={() => handleNevigate()} className="bg-blue-400 cursor-pointer"/>
+        </div>
       </div>
-      <div className="h-14"></div>
       <Suggestions />
     </div>
   );
