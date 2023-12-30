130119c4c7a840fdecfc465beca2ed44ea870b4c
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-12-30 10:22:43 +0530

    header & suggestions : did some improvement

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { IoIosSearch, IoIosNotificationsOutline } from "react-icons/io";
 import { FaRegWindowClose } from "react-icons/fa";
-import { IoArrowBackSharp } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { BsMicFill } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -19,6 +18,7 @@ const Header = () => {
   const [mobileSearch, setMobileSearch] = useState(false);
   const navigate = useNavigate();
 
+  // Function to handle search query on Enter press
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       if (searchQuery !== "") {
@@ -28,54 +28,56 @@ const Header = () => {
     }
   };
 
-  const handleNevigate = () => {
-    if (searchQuery !== "") {
-      navigate(`/search/${searchQuery}`);
+  // Function to navigate on search icon click
+  const handleNavigate = (query) => {
+    if (query !== "") {
+      navigate(`/search/${query}`);
       dispatch(setSearchQuery(""));
     }
   };
 
   return (
-    <div className="h-10 md:h-12">
-      <div
-        className={`fixed left-0 top-0 z-30 flex w-full items-center justify-between   px-4 py-3  ${
+    <header className={`mx-auto  flex h-10 flex-col items-center md:h-12`}>
+      {/* Navbar */}
+      <nav
+        className={`fixed left-0 top-0 z-30 flex w-full items-center justify-between bg-white px-4 py-3 ${
           mobileSearch && "hidden"
         }`}
       >
-        {/* Column 1 */}
-        <div className={` flex  space-x-9 ${menu && " space-x-9 "} `}>
+        {/* Col 1 */}
+        <section className={`flex space-x-9 ${menu && " space-x-9 "}`}>
           {menu ? (
             <FaRegWindowClose
               size={28}
               onClick={() => setMenu((prev) => !prev)}
-              className=" z-50 cursor-pointer"
+              className="z-50 cursor-pointer"
             />
           ) : (
             <RxHamburgerMenu
               size={26}
               onClick={() => setMenu((prev) => !prev)}
-              className=" z-50 cursor-pointer"
+              className="z-50 cursor-pointer"
             />
           )}
-          <NavLink to="/" className=" z-50 w-24">
+          <NavLink to="/" className="z-50 w-24">
             <img src="/assets/logo.svg" alt="logo" />
           </NavLink>
-        </div>
+        </section>
 
-        {/* column 2 */}
-        <div className=" hidden w-2/5 items-center space-x-4 md:flex">
-          <div className="search-container flex h-11  w-full items-center rounded-3xl border-2 border-gray-300">
+        {/* Col 2 */}
+        <section className="hidden w-2/5 items-center space-x-4 md:flex">
+          <div className="search-container flex h-11 w-full items-center rounded-3xl border-2 border-gray-300">
             <input
               type="text"
               placeholder="Search"
-              className="mr-px flex h-full  w-full  rounded-l-3xl    px-4 "
+              className="mr-px flex h-full w-full rounded-l-3xl px-4"
               onChange={(e) => dispatch(setSearchQuery(e.target.value))}
               value={searchQuery}
               onKeyDown={handleKeyDown}
             />
             <div
-              onClick={handleNevigate}
-              className=" flex h-full  items-center justify-center  rounded-r-3xl  border-gray-300 bg-gray-300 px-4"
+              onClick={()=>handleNavigate(searchQuery)}
+              className="flex h-full items-center justify-center rounded-r-3xl border-gray-300 bg-gray-300 px-4"
             >
               <IoIosSearch size={26} className="cursor-pointer" />
             </div>
@@ -83,17 +85,15 @@ const Header = () => {
           <div className="flex cursor-pointer items-center justify-center rounded-full bg-gray-300 p-3 text-sm">
             <BsMicFill size={20} />
           </div>
-        </div>
+        </section>
 
-        {/* column 3 */}
-
-        <div className=" flex items-center space-x-4">
+        {/* col 3 */}
+        <section className="flex items-center space-x-4">
           <IoIosSearch
             size={26}
             className="cursor-pointer md:hidden"
             onClick={() => setMobileSearch((prev) => !prev)}
           />
-
           <BsMicFill
             size={24}
             className="hidden cursor-pointer sm:block md:hidden"
@@ -103,7 +103,6 @@ const Header = () => {
             alt="add video"
             className="cursor-pointer"
           />
-
           <IoIosNotificationsOutline
             className="hidden cursor-pointer sm:flex"
             size={26}
@@ -113,24 +112,25 @@ const Header = () => {
             alt="user icon"
             className="w-[30px] cursor-pointer"
           />
-        </div>
-
+        </section>
         {menu && <Hamburger />}
-      </div>
-      {/* this part is for mobile device only */}
-      <div
-        className={`z-50  mx-4   mt-2 flex  h-9 items-center rounded-3xl ${
+      </nav>
+
+      {/* For Mobile Searchbar*/}
+      <section
+        className={`fixed left-0 top-0 z-50 mx-4 mt-2 flex h-9 items-center rounded-3xl ${
           !mobileSearch && "hidden"
         }`}
       >
         <IoIosArrowRoundBack
           size={40}
           onClick={() => setMobileSearch((prev) => !prev)}
+          className="cursor-pointer"
         />
         <div className="ml-2 flex h-full w-full items-center space-x-3 rounded-full border-2 bg-gray-200 pr-3">
           <input
             type="text"
-            className="w-4/5 rounded-md bg-transparent  px-4  py-[2px] outline-none focus:border-gray-600 "
+            className="w-4/5 rounded-md bg-transparent px-4 py-[2px] outline-none focus:border-gray-600"
             placeholder="Search"
             onChange={(e) => dispatch(setSearchQuery(e.target.value))}
             value={searchQuery}
@@ -139,17 +139,23 @@ const Header = () => {
           <IoMdClose
             size={22}
             onClick={() => dispatch(setSearchQuery(""))}
-            className=" cursor-pointer"
+            className="cursor-pointer"
           />
           <IoIosSearch
             size={22}
-            onClick={() => handleNevigate()}
-            className=" cursor-pointer"
+            onClick={() => handleNavigate(searchQuery)}
+            className="cursor-pointer"
           />
         </div>
-      </div>
-      <Suggestions />
-    </div>
+      </section>
+
+      {/* for suggestion section */}
+      <section
+        className={`fixed   z-50  mt-12 w-full bg-white sm:mt-[53px] sm:w-[70%] md:mt-[65px] md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[38%] rounded-xl`}
+      >
+        <Suggestions />
+      </section>
+    </header>
   );
 };
 
--- src/components/search/Suggestions.jsx
+++ src/components/search/Suggestions.jsx
@@ -46,36 +46,29 @@ const Suggestions = () => {
     dispatch(setSearchQuery(""));
   };
 
-  const closeSuggestions = () => {
-    dispatch(setSearchQuery(""));
-  };
-
   return (
-    <section onClick={() => closeSuggestions()}>
-      {/* search suggestions */}
+    <>
       {searchQuery && suggestions && (
-        <div className="fixed top-0 z-50 flex h-fit w-screen justify-center">
-          <div className="mt-[50px] w-full space-y-3 bg-white p-6 shadow-lg sm:w-[70%] sm:rounded-2xl md:mt-[60px] md:w-[60%] lg:w-[50%] xl:w-[40%]">
-            {suggestions?.items?.map((suggestion, index) => {
-              return (
-                <div
-                  onClick={(e) =>
-                    handleNavigate(`/search/${suggestion.snippet.title}`, e)
-                  }
-                  className="flex cursor-pointer items-center gap-3 font-semibold"
-                  key={index}
-                >
-                  <p>
-                    <IoIosSearch size={20} />
-                  </p>
-                  <p className="line-clamp-1">{suggestion.snippet.title}</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <section className={`mx-4 space-y-2 py-4`}>
+          {suggestions?.items?.map((suggestion, index) => {
+            return (
+              <div
+                onClick={(e) =>
+                  handleNavigate(`/search/${suggestion.snippet.title}`, e)
+                }
+                className="flex cursor-pointer items-center gap-3 font-semibold"
+                key={index}
+              >
+                <p>
+                  <IoIosSearch size={20} />
+                </p>
+                <p className="line-clamp-1">{suggestion.snippet.title}</p>
+              </div>
+            );
+          })}
+        </section>
       )}
-    </section>
+    </>
   );
 };
 
