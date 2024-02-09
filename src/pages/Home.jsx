837b55a1afa011d9dd150d218a9dd77481390ed7
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-02-09 10:26:47 +0530

    home done: pretify the code and added endMessage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,12 @@ import HomeSkeleton from "../components/skeletons/HomeSkeleton";
 import HomeThumnailCard from "../components/home/HomeThumnailCard";
 
 const Home = () => {
+  // Using useInfiniteQuery to fetch data in an infinite-scrolling manner
   const { data, fetchNextPage, hasNextPage, error, isLoading } =
     useInfiniteQuery({
       queryKey: ["trending"],
       queryFn: ({ pageParam = "" }) =>
+        // Fetching YouTube data with specified parameters
         getYouTubeData({
           endpoint: "videos",
           queryParams: {
@@ -20,25 +22,37 @@ const Home = () => {
             regionCode: "IN",
           },
         }),
-      getNextPageParam: (lastPage) => lastPage.nextPageToken,
-      staleTime: 1000 * 60 * 5,
+      getNextPageParam: (lastPage) => lastPage.nextPageToken, // Function to get the next page token
+      staleTime: 1000 * 60 * 5, // 5min Stale time
     });
 
+  // Flattening the data pages to get an array of videos
   const videos = data?.pages.flatMap((page) => page.items) || [];
 
   return (
     <section>
-      {error && <div>Error: {error.message}</div>}
+      {/* Displaying error message if there's an error */}
+      {error && (
+        <div className="ml-5 mt-6 text-red-500">Error: {error.message}</div>
+      )}
+      {/* Infinite scrolling component */}
       <InfiniteScroll
         dataLength={videos.length}
         next={fetchNextPage}
         hasMore={hasNextPage}
+        endMessage={
+          <div className="pb-8 pl-7 pt-3 text-xl text-red-500 ">
+            No more videos available
+          </div>
+        }
       >
         <div className="m-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {/* Mapping through videos and rendering thumbnail cards */}
           {videos &&
             videos.map((video, index) => (
               <HomeThumnailCard key={video.id + index} video={video} />
             ))}
+          {/* Rendering skeleton components while loading */}
           {isLoading &&
             Array.from({ length: 20 }).map((_, index) => (
               <HomeSkeleton key={index} />
