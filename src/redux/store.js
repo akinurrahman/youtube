86f2a43bb7bd2a6240f08606ac8dcd8a92ecfd12
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-02-15 17:50:23 +0530

    finally replaced all rtk query or useFetch with react query

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { youtubeService } from "../api/youtubeService";
 import infoReducer from "./features/infoSlice";
 const store = configureStore({
   reducer: {
     info: infoReducer,
-    [youtubeService.reducerPath]: youtubeService.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(youtubeService.middleware),
 });
 
 export default store;
